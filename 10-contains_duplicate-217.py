cd843599bee66aea9318a706f274ba5e0a31ee55
felipe-rods <felipe.git@outlook.com>
2025-07-21 20:31:22 -0300

    Leetcode 217: constains duplicate

---
+++ 10-contains_duplicate-217.py
@@ -0,0 +1,17 @@
+class Solution:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
+
+
+
+solution = Solution()
+print(solution.containsDuplicate([1, 2, 3, 4]))
+print(solution.containsDuplicate([1, 1, 2]))
+print(solution.containsDuplicate([1, 3, 1]))
+print(solution.containsDuplicate([1, 4, 3, 2]))
+print(solution.containsDuplicate([2, 3]))
