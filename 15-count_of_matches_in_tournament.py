18dfbb254238822ec7b0a1ab9f489cb6f4bd6ed2
Felipe Rodrigues <felipe.git@outlook.com>
2026-03-12 13:10:40 -0300

    leetcode 1688:count of matches in tournament

---
+++ 15-count_of_matches_in_tournament.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        matches = 0
+        while n > 1:
+            ref = n // 2
+            matches += ref
+            n -= ref
+        return matches
+
+
+solution = Solution()
+print(solution.numberOfMatches(7))
