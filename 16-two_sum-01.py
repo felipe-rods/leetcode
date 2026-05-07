4eea446a54cb6cd147f64e697bc79674defe4f48
Felipe Rodrigues <felipe.git@outlook.com>
2026-05-07 15:37:26 -0300

    Leetcode 205: isomorphic strings

--- 16-two_sum-01.py
+++ 16-two_sum-01.py
@@ -2,9 +2,13 @@ class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         pair_index = {}
         
+        # Para o índice e o número atual da lista
         for index, num in enumerate(nums):
+            # Se o alvo menos o número está na pair_index:
             if target - num in pair_index:
+                # retorna índice e o índice par do alvo - número
                 return [index, pair_index[target - num]]
+            # caso contrário, adiciona o número e o índice ao pair_index
             pair_index[num] = index
 
                  
