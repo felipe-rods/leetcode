7b7793ccc4d52ae7e4026b96c50a718f427c2d42
Felipe Rodrigues <felipe.git@outlook.com>
2026-05-12 18:23:17 -0300

    Leetcode 202: Happy Number

---
+++ 19-happy_number-202.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        numbers = []
+        digit = 0
+        total = 0
+        while n != 1:
+            while n > 0:
+                digit = n % 10
+                total += digit ** 2
+                n //= 10
+            if total in numbers:
+                return False
+            numbers.append(total)
+            n = total
+            total = 0
+        return True
+
+
+solution = Solution()
+print(solution.isHappy(1))
+print(solution.isHappy(70))
+print(solution.isHappy(10))
+print(solution.isHappy(13))
+print(solution.isHappy(49))
+print(solution.isHappy(97))
+print(solution.isHappy(28))
+print(solution.isHappy(86))
